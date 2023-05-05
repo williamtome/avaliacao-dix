ff243819c7bb145681f30c2d90553db46c0f6c08
William Tomé <william.tomenh@gmail.com>
2023-05-05 17:07:38 -0300

    feat(view/success): added close button.

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,5 +1,6 @@
 @if (session($key ?? 'status'))
     <div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
         {{ session($key ?? 'status') }}
     </div>
 @endif
